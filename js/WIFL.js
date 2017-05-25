d18ac8f44ab37adf6e2f52a6af03c35174a3fc21
Rosita <windair357@gmail.com>
2017-05-25 16:54:02 -0400

    (mostly) Final Submission of Project 5/25/2017
    This is the (near) final submission of my final project!
    
    Content needs a bit of update and there’s a few css bugs I’d like to
    fix. Moises helped me a lot with the flip function!
    
    Hamburger menus are also pretty straightforward, and I used a code pen
    from someone to help me template it.

--- js/WIFL.js
+++ js/WIFL.js
@@ -1,10 +1,23 @@
  // JS drop down (hamburger) menu to get to different sections of the website
-$(document).ready(function() {
-    $(this).on('click', '#menu-button', function(event) {
-        event.preventDefault();
-        $(this).toggleClass('visible');
-    })
-});
+//$(document).ready(function(){
+
+   
+$(".cross").hide();
+$(".menu").hide();
+
+$(".hamburger").click(function() {
+	$(".menu").slideToggle( "slow", function() {
+		$(".hamburger").hide();
+		$(".cross").show();
+			});
+		});
+
+$(".cross").click(function() {
+	$(".menu").slideToggle("slow", function() {
+		$(".cross").hide();
+		$(".hamburger").show();
+		});
+	});
 
 // JS when an image is clicked the information about that illness through flipping appears;
 
